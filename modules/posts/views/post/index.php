8491f636858e14ed6ecfe82b11669441d0077a2a
Philipp Frenzel <philipp@frenzel.net>
2014-04-19 19:14:21 +0200

    enhancing overbie

--- modules/posts/views/post/index.php
+++ modules/posts/views/post/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Url;
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use kartik\grid\GridView;
 use kartik\widgets\SideNav;
 
@@ -17,11 +18,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <?php yii\widgets\Block::begin(array('id'=>'sidebar')); ?>
 
-	<?php 
+  <?php 
 
-  	$sideMenu = array();
-  	$sideMenu[] = array('icon'=>'home','label'=>Yii::t('app','Home'),'url'=>Url::to(array('/site/index')));
-  	$sideMenu[] = array('icon'=>'plus','label'=>Yii::t('app','New Post'),'url'=>Url::to(array('/posts/post/create')));
+    $sideMenu = array();
+    $sideMenu[] = array('icon'=>'home','label'=>Yii::t('app','Home'),'url'=>Url::to(array('/site/index')));
+    $sideMenu[] = array('icon'=>'plus','label'=>Yii::t('app','New Post'),'url'=>Url::to(array('/posts/post/create')));
     $sideMenu[] = array('icon'=>'arrow-right','label'=>Yii::t('app','Manage Categories'),'url'=>Url::to(array('/categories/categories/index')));
     $sideMenu[] = array('icon'=>'arrow-right','label'=>Yii::t('app','Manage Tags'),'url'=>Url::to(array('/tags/default/index')));
    
@@ -40,13 +41,13 @@ $this->params['breadcrumbs'][] = $this->title;
 
   <h1 class="page-header"><?= Html::encode($this->title) ?></h1>  
 
-	<?php echo GridView::widget([
-		'dataProvider' => $dataProvider,
-		'filterModel' => $searchModel,
-		'columns' => [
-			['class' => 'yii\grid\SerialColumn'],
-			//'id',
-			'title',
+  <?php echo GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+      ['class' => 'yii\grid\SerialColumn'],
+      //'id',
+      'title',
       [
         'attribute'=>'author_id',
         'value'=>function ($model, $index, $widget) { 
@@ -75,33 +76,33 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
         'filterInputOptions' => ['placeholder' => 'All Stati'],
       ],
-			//'content:html',
+      //'content:html',
       [
         'attribute'=>'categories_id',
         'value'=>function ($model, $index, $widget) { 
             return Html::tag('div',$model->category->name);
         },
         'filterType' => GridView::FILTER_SELECT2,
-        'filter'=>\app\modules\categories\models\Categories::pdCategories(), 
+        'filter'=>ArrayHelper::merge('0'=>'none',\app\modules\categories\models\Categories::pdCategories()), 
         'filterWidgetOptions'=>[
             'pluginOptions' => ['allowClear' => true],
         ],
         'filterInputOptions' => ['placeholder' => 'All Categories'],
         'format'=>'raw'
       ],
-			'tags:ntext', 
-			// 
-			// 'time_update:datetime',
+      'tags:ntext', 
+      // 
+      // 'time_update:datetime',
       'time_create:datetime',      
-			['class' => 'kartik\grid\ActionColumn'],
-		],
-		'panel' => [
+      ['class' => 'kartik\grid\ActionColumn'],
+    ],
+    'panel' => [
         'heading'=>'<h3 class="panel-title"><i class="glyphicon glyphicon-globe"></i> Posts</h3>',
         'type'=>'success',
         'before'=>Html::a('<i class="glyphicon glyphicon-plus"></i> Create Post', ['create'], ['class' => 'btn btn-success']),
         'after'=>Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset Grid', ['index'], ['class' => 'btn btn-info']),
         'showFooter'=>false
     ],
-	]); ?>
+  ]); ?>
 
 </div>

8e5ed495c9c25f1c84cf3e5801be8dbfb7264474
Philipp Frenzel <philipp@frenzel.net>
2014-08-10 11:13:21 +0200

    fix assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -5,33 +5,41 @@
  * @license http://www.yiiframework.com/license/
  */
 
-/**
- * @author Philipp Frenzel <philipp@frenzel.net>
- */
-
 namespace app\assets;
 
 use yii\web\AssetBundle;
 
 /**
- * @author Original Qiang Xue <qiang.xue@gmail.com>
+ * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
 class AppAsset extends AssetBundle
 {
-	public $basePath = '@webroot';
-	public $baseUrl = '@web';
-	public $css = [
-		'css/styles.css',
-		'css/site.css'
-	];
-	public $js = [
-		'js/scripts.js',
-		//'js/excells/excell_modal_button.js'
-	];
-	public $depends = [
-		'yii\web\YiiAsset',
-		'yii\bootstrap\BootstrapAsset',
-		'frenzelgmbh\appcommon\commonAsset'
-	];
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [        
+        'amoeba/css/isotope.css',
+        'amoeba/js/fancybox/jquery.fancybox.css',
+        'amoeba/css/animate.css',        
+        'amoeba/css/style.css',
+        'amoeba/skin/default.css' 
+    ];
+    public $js = [        
+        'amoeba/js/modernizr-2.6.2-respond-1.1.0.min.js',
+        'amoeba/js/jquery.appear.js',
+        'amoeba/js/jquery.easing.1.3.js',
+        'amoeba/js/jquery.isotope.min.js',
+        'amoeba/js/jquery.scrollTo-1.4.3.1-min.js',
+        'amoeba/js/jquery.nicescroll.min.js',
+        'amoeba/js/jquery.localscroll-1.2.7-min.js',
+        'amoeba/js/fancybox/jquery.fancybox.pack.js',
+        'amoeba/js/skrollr.min.js',        
+        'amoeba/js/stellar.js',
+        'amoeba/js/main.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'frenzelgmbh\appcommon\commonAsset'
+    ];
 }

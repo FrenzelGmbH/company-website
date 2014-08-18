0b3863f2f51c843ad03dde31fb5df9fb4170dab6
Philipp Frenzel <philipp@frenzel.net>
2014-08-18 22:00:40 +0200

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -233,8 +233,8 @@ $this->title = 'Frenzel GmbH - Business Intelligence, Analysis and Planning';
 <!-- spacer section:parallax1 -->
     <section id="parallax1" class="section pad-top40 pad-bot40" data-stellar-background-ratio="0.5">
         <div class="container">
-        <div class="align-center pad-top40 pad-bot40">
-            <blockquote class="bigquote color-white">Blog</blockquote>
+        <div class="pad-top40 pad-bot40">
+            <blockquote class="bigquote">Blog</blockquote>
             <?php 
               if(class_exists('frenzelgmbh\sblog\widgets\PortletPostsStyled')){
                 echo frenzelgmbh\sblog\widgets\PortletPostsStyled::widget([
